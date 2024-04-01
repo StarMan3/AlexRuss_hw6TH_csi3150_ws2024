9c5dc77a3ed4aecd54f40c58b925079b69a0b203
Alex <98064532+StarMan3@users.noreply.github.com>
2024-03-31 21:31:41 -0400

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Hero.css'
+import HeroImage from '../../Assets/HeroSection-bg-image.jpg'
 
 function Hero() {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <section className='hero-section'>
+            <img className='hero-image' src={HeroImage} alt='Paper Airplane in the Sunset' />
+
+        </section>
+    )
 }
 
 export default Hero
